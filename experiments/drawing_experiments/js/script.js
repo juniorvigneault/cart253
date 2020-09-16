f5a5123363fb692bac0e9d76fd23b8382a71bc22
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-16 19:14:50 -0400

    drew a rectangle

--- experiments/drawing_experiments/js/script.js
+++ experiments/drawing_experiments/js/script.js
@@ -9,6 +9,13 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(500,500);
+
+background(200,50,50);
+
+point(50,50);
+
+rect(0,0,250,250);
 
 }
 
