193e0c9e7d551cb5c15e1070413317c37546f317
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-22 19:40:47 -0400

    played with variables

--- experiments/variable_experiments/js/script.js
+++ experiments/variable_experiments/js/script.js
@@ -9,12 +9,18 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+background(mouseX,20,2);
+
+
+rectMode(CENTER);
+rect(width/3,height/3,100)
+
 
 }
