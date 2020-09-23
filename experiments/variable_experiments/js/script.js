fc7249cd1d1ed57992a10905d491fc01d43c1c3a
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-23 11:07:49 -0400

    Played with variables, map and constrain

--- experiments/variable_experiments/js/script.js
+++ experiments/variable_experiments/js/script.js
@@ -5,22 +5,31 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let backgroundColor = 0;
+let circle={
+  x:0,
+  y:250,
+  size:100,
+  speed:1,
+  insideColor:255
+}
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-createCanvas(windowWidth,windowHeight);
+createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-background(mouseX,20,2);
+background(backgroundColor);
 
+circle.x=circle.x+circle.speed;
 
-rectMode(CENTER);
-rect(width/3,height/3,100)
-
+circle.insideColor = map(mouseX,0,500,0,255);
+fill(circle.insideColor);
+ellipse(circle.x,circle.y,circle.size);
 
 }
