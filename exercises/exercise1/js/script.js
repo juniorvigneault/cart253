cc8728781f09ade8cce68bc8ef4a058dc71f0e0a
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-23 21:42:39 -0400

    E1: Created a background

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -1,14 +1,27 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Exercise 01 : I like to move it move it
+Junior Vigneault - 40136480
+
 
 Here is a description of this template p5 project.
 **************************************************/
 
+let backgroundColor = 0
+
+let square1 = {
+  x: 500,
+  y: 500,
+  width: 1000,
+  height: 1000,
+  fill:0
+
+
+}
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(1000,1000);
 
 }
 
@@ -16,5 +29,12 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundColor)
+
+rectMode(CENTER)
+noStroke()
+rect(square1.x,square1.y,square1.width,square1.height);
+fill(square1.fill);
+
 
 }
