3ecc8e8ca0bfd9b50f4eac12ff86c86f77dcfea5
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-23 22:10:07 -0400

    A1: Again, starting over except canvas size

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -6,6 +6,15 @@ Junior Vigneault - 40136480
 Here is a description of this template p5 project.
 **************************************************/
 
+let square1 = {
+  x: 500,
+  y: 500,
+  height: 1000,
+  width: 1000,
+  fill: 255,
+  alpha: 50
+}
+
 // setup()
 //
 // Description of setup() goes here.
@@ -18,8 +27,13 @@ createCanvas(1000,1000);
 //
 // Description of draw() goes here.
 function draw() {
+background(255,0,0);
 
+// drawing square to make strobe effect
+rectMode(CENTER);
 
+rect(square1.x,square1.y,square1.height,square1.width);
+fill(square1.fill = random(0,255));
 
 
 
