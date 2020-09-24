4948b9be94dc29ee615aa7aae67829062002d4be
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-23 21:56:01 -0400

    E1: starting over :(

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -6,17 +6,6 @@ Junior Vigneault - 40136480
 Here is a description of this template p5 project.
 **************************************************/
 
-let backgroundColor = 0
-
-let square1 = {
-  x: 500,
-  y: 500,
-  width: 1000,
-  height: 1000,
-  fill:0
-
-
-}
 // setup()
 //
 // Description of setup() goes here.
@@ -29,12 +18,9 @@ createCanvas(1000,1000);
 //
 // Description of draw() goes here.
 function draw() {
-  background(backgroundColor)
 
-rectMode(CENTER)
-noStroke()
-rect(square1.x,square1.y,square1.width,square1.height);
-fill(square1.fill);
+
+
 
 
 }
