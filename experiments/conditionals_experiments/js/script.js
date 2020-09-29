eafb32f8359b6fe539bf39c29035b826093b9a0b
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-29 19:26:48 -0400

    More experiments with conditionals

--- experiments/conditionals_experiments/js/script.js
+++ experiments/conditionals_experiments/js/script.js
@@ -1,20 +1,37 @@
-/**************************************************
-Template p5 project
-Pippin Barr
 
-Here is a description of this template p5 project.
-**************************************************/
+let backgroundColor = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 1
+}
 
-// setup()
-//
-// Description of setup() goes here.
+
 function setup() {
+  createCanvas(500,500);
 
 }
 
-// draw()
-//
-// Description of draw() goes here.
+
+
 function draw() {
+  background(backgroundColor);
+
+  circle.x = circle.x + circle.speed;
+
+if (mouseX < width/3) {
+  fill(255,0,0);
+}
+else if (mouseX < 2 * width/3) {
+  fill(0,255,0);
+}
+else {
+  fill(0,0,255);
+}
+ellipse(circle.x, circle.y, circle.size);
+
+
+
 
 }
