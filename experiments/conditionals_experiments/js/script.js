89b6949ec8b21707ec2ba464fa67f1e1e641022c
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-09-30 13:08:04 -0400

    A4: started dodging covid 19 activity

--- experiments/conditionals_experiments/js/script.js
+++ experiments/conditionals_experiments/js/script.js
@@ -1,12 +1,6 @@
 
-let backgroundColor = 0;
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  speed: 1
-}
 
+let angle = 0;
 
 function setup() {
   createCanvas(500,500);
@@ -16,22 +10,7 @@ function setup() {
 
 
 function draw() {
-  background(backgroundColor);
-
-  circle.x = circle.x + circle.speed;
-
-if (mouseX < width/3) {
-  fill(255,0,0);
-}
-else if (mouseX < 2 * width/3) {
-  fill(0,255,0);
-}
-else {
-  fill(0,0,255);
-}
-ellipse(circle.x, circle.y, circle.size);
-
-
+background(0);
 
 
 }
