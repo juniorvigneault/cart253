cb5cf9abed6864c1a21d9a0fcdf3c4aad99edc70
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-01 11:07:42 -0400

    A2: Changed background to club strobe

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -12,7 +12,13 @@ function preload() {
   flyImage = loadImage("assets/images/fly.png");
 }
 
-let covid19 ={
+let bg = {
+  r:0,
+  g:0,
+  b:0
+}
+
+let covid19 = {
   x:0,
   y:250,
   size:250,
@@ -41,7 +47,12 @@ createCanvas(windowWidth, windowHeight);
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+  bg.r = random(200,255);
+  bg.g = random(200,555);
+  bg.b = random(200,555);
+
+  background(bg.r, bg.g, bg.b);
+
 
 for (let i =0; i < 10000; i++) {
   let x = random(0,width);
@@ -62,10 +73,6 @@ for (let i =0; i < 10000; i++) {
   flyImage.x = mouseX;
   flyImage.y = mouseY;
 
-  let d = dist(flyImage.x,flyImage.y,covid19.x,covid19.y);
-  if (d < covid19.size/2 + flyImage.size) {
-    noLoop();
-}
 
   fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
   ellipse(covid19.x, covid19.y, covid19.size);
