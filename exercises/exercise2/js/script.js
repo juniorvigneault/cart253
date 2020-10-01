eb6b5c684621ce878fce69fbc61cba58d98449bd
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-01 17:38:13 -0400

    A2: the fly can now pick up the martini!!

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -37,10 +37,20 @@ let discoBallImage2 = {
 let discoBallImage3 = {
   x:0,
   y:0,
-  size:100,
+  size:300,
   vx:0,
   vy:0,
-  speed:6,
+  speed:7,
+  image: undefined
+}
+
+let discoBallImage4 = {
+  x:0,
+  y:0,
+  size:200,
+  vx:0,
+  vy:0,
+  speed:1,
   image: undefined
 }
 
@@ -48,8 +58,11 @@ let martiniImage = {
   x:0,
   y:0,
   size:100,
-  image:undefined
+  image:undefined,
+  beingCarried:false
 }
+
+let clubSong;
 
 // Loading the images! Finally...
 
@@ -60,6 +73,10 @@ function preload() {
   discoBallImage.image = loadImage("assets/images/discoball.png");
 
   discoBallImage2.image = loadImage("assets/images/discoball.png");
+
+  discoBallImage3.image = loadImage("assets/images/discoball.png");
+
+  discoBallImage4.image = loadImage("assets/images/discoball.png");
 
   martiniImage.image = loadImage("assets/images/martini.png")
 }
@@ -86,10 +103,18 @@ createCanvas(windowWidth, windowHeight);
   discoBallImage2.y = random(0,height);
   discoBallImage2.vx = discoBallImage2.speed;
 
+  discoBallImage3.y = random(0,height);
+  discoBallImage3.vx = discoBallImage3.speed;
+
+  discoBallImage4.y = random(0,height);
+  discoBallImage4.vx = discoBallImage4.speed;
+
   noCursor();
 
-}
+martiniImage.x = width/2;
+martiniImage.y = height/2;
 
+}
 // draw()
 //
 // Description of draw() goes here.
@@ -114,8 +139,12 @@ for (let i =0; i < 1000; i++) {
 
 // Dropping a martini cocktail in the middle of the screen to be picked up by the fly
 
-  martiniImage.x = width/2;
-  martiniImage.y = height/2;
+if (martiniImage.beingCarried){
+  martiniImage.x = flyImage.x;
+  martiniImage.y = flyImage.y;
+
+}
+
 
   image(martiniImage.image, martiniImage.x, martiniImage.y, martiniImage.size, martiniImage.size+20);
 
@@ -126,6 +155,12 @@ for (let i =0; i < 1000; i++) {
 
   discoBallImage2.x = discoBallImage2.x + discoBallImage2.vx;
   discoBallImage2.y = discoBallImage2.y + discoBallImage2.vy;
+
+  discoBallImage3.x = discoBallImage3.x + discoBallImage3.vx;
+  discoBallImage3.y = discoBallImage3.y + discoBallImage3.vy;
+
+  discoBallImage4.x = discoBallImage4.x + discoBallImage4.vx;
+  discoBallImage4.y = discoBallImage4.y + discoBallImage4.vy;
 
   // If the disco balls hits the right of the screen, it reappears on the right
 
@@ -139,12 +174,28 @@ for (let i =0; i < 1000; i++) {
     discoBallImage2.y = random(0,height);
   }
 
+  if (discoBallImage3.x > width) {
+    discoBallImage3.x = 0;
+    discoBallImage3.y = random(0,height);
+  }
+
+  if (discoBallImage4.x > width) {
+    discoBallImage4.x = 0;
+    discoBallImage4.y = random(0,height);
+  }
+
 // Making the disco ball appear on screen
   imageMode(CENTER);
   image(discoBallImage.image, discoBallImage.x, discoBallImage.y, discoBallImage.size, discoBallImage.size);
 
   imageMode(CENTER);
   image(discoBallImage2.image, discoBallImage2.x, discoBallImage2.y, discoBallImage2.size, discoBallImage2.size);
+
+  imageMode(CENTER);
+  image(discoBallImage3.image, discoBallImage3.x, discoBallImage3.y, discoBallImage3.size, discoBallImage3.size);
+
+  imageMode(CENTER);
+  image(discoBallImage4.image, discoBallImage4.x, discoBallImage4.y, discoBallImage4.size, discoBallImage4.size);
 
 // Making the fly follow the user's mouse
 
@@ -156,10 +207,25 @@ for (let i =0; i < 1000; i++) {
   imageMode(CENTER);
   image(flyImage.image, flyImage.x, flyImage.y, flyImage.size, flyImage.size);
 
-// determine the distance between the fly and discoball and make the program stop when they touch
+// determine the distances between the fly and discoballs and make the program stop when they touch
 
   let d = dist(flyImage.x, flyImage.y, discoBallImage.x, discoBallImage.y);
   if (d < discoBallImage.size/3 + flyImage.size/2) {
+    noLoop();
+  }
+
+  let d2 = dist(flyImage.x, flyImage.y, discoBallImage2.x, discoBallImage2.y);
+  if (d2 < discoBallImage2.size/3 + flyImage.size/2) {
+    noLoop();
+  }
+
+  let d3 = dist(flyImage.x, flyImage.y, discoBallImage3.x, discoBallImage3.y);
+  if (d3 < discoBallImage3.size/3 + flyImage.size/2) {
+    noLoop();
+  }
+
+  let d4 = dist(flyImage.x, flyImage.y, discoBallImage4.x, discoBallImage4.y);
+  if (d4 < discoBallImage4.size/3 + flyImage.size/2) {
     noLoop();
   }
 
@@ -172,5 +238,20 @@ for (let i =0; i < 1000; i++) {
   else {
     discoBallImage.size = 300;
     discoBallImage.speed = 8;
+  }
+}
+
+// picking up the martini
+
+function mousePressed(){
+  if (martiniImage.beingCarried){
+    martiniImage.beingCarried = false;
+  }
+
+  else {
+  let d = dist(flyImage.x, flyImage.y, martiniImage.x, martiniImage.y);
+  if (d < martiniImage.size) {
+    martiniImage.beingCarried = true;
+  }
   }
 }
