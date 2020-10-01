d34685f2d9e618f502f487461e0821d1f2a7fa5b
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-01 16:46:00 -0400

    A2: Added martini

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -7,7 +7,13 @@ A fly in a club can pick up cocktails but needs to dodge disco balls
 
 // Declaring my images for the program, a fly image for the user and a discoball as the obstacle
 
-let flyImage;
+let flyImage = {
+  x:0,
+  y:0,
+  size:80,
+  image:undefined
+}
+
 let discoBallImage = {
   x:0,
   y:0,
@@ -18,12 +24,44 @@ let discoBallImage = {
   image: undefined
 }
 
+let discoBallImage2 = {
+  x:0,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:6,
+  image: undefined
+}
+
+let discoBallImage3 = {
+  x:0,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:6,
+  image: undefined
+}
+
+let martiniImage = {
+  x:0,
+  y:0,
+  size:100,
+  image:undefined
+}
+
 // Loading the images! Finally...
 
 function preload() {
-  flyImage = loadImage("assets/images/fly.png");
+
+  flyImage.image = loadImage("assets/images/fly.png");
 
   discoBallImage.image = loadImage("assets/images/discoball.png");
+
+  discoBallImage2.image = loadImage("assets/images/discoball.png");
+
+  martiniImage.image = loadImage("assets/images/martini.png")
 }
 
 // Creating a javascript object for my background to make the colors funky
@@ -34,27 +72,30 @@ let bg = {
   b:0
 }
 
-
 function setup() {
 
   // I want the biggesssssst canvas
 
 createCanvas(windowWidth, windowHeight);
 
-  // Making the disco ball randomly appear on the left
+  // Making the disco balls randomly appear on the left
 
   discoBallImage.y = random(0,height);
   discoBallImage.vx = discoBallImage.speed;
 
-  noCursor();
+  discoBallImage2.y = random(0,height);
+  discoBallImage2.vx = discoBallImage2.speed;
 
+  noCursor();
 
 }
 
 // draw()
 //
 // Description of draw() goes here.
+
 function draw() {
+
   bg.r = random(200,255);
   bg.g = random(200,555);
   bg.b = random(200,555);
@@ -67,25 +108,43 @@ for (let i =0; i < 1000; i++) {
   let x = random(0,width);
   let y = random(0,height);
   stroke(255);
-  strokeWeight(20);
+  strokeWeight(2);
   point(x,y);
 }
+
+// Dropping a martini cocktail in the middle of the screen to be picked up by the fly
+
+  martiniImage.x = width/2;
+  martiniImage.y = height/2;
+
+  image(martiniImage.image, martiniImage.x, martiniImage.y, martiniImage.size, martiniImage.size+20);
 
 // The disco ball is moving accross the screen using it's velocity
 
   discoBallImage.x = discoBallImage.x + discoBallImage.vx;
   discoBallImage.y = discoBallImage.y + discoBallImage.vy;
 
-  // If the disco ball hits the right of the screen, it reappears on the right
+  discoBallImage2.x = discoBallImage2.x + discoBallImage2.vx;
+  discoBallImage2.y = discoBallImage2.y + discoBallImage2.vy;
+
+  // If the disco balls hits the right of the screen, it reappears on the right
 
   if (discoBallImage.x > width) {
     discoBallImage.x = 0;
     discoBallImage.y = random(0,height);
   }
 
+  if (discoBallImage2.x > width) {
+    discoBallImage2.x = 0;
+    discoBallImage2.y = random(0,height);
+  }
+
 // Making the disco ball appear on screen
   imageMode(CENTER);
-  image(discoBallImage.image, discoBallImage.x, discoBallImage.y, 300, 300);
+  image(discoBallImage.image, discoBallImage.x, discoBallImage.y, discoBallImage.size, discoBallImage.size);
+
+  imageMode(CENTER);
+  image(discoBallImage2.image, discoBallImage2.x, discoBallImage2.y, discoBallImage2.size, discoBallImage2.size);
 
 // Making the fly follow the user's mouse
 
@@ -95,6 +154,23 @@ for (let i =0; i < 1000; i++) {
 // Making the fly appear on screen
 
   imageMode(CENTER);
-  image(flyImage, mouseX, mouseY,80,80);
+  image(flyImage.image, flyImage.x, flyImage.y, flyImage.size, flyImage.size);
 
+// determine the distance between the fly and discoball and make the program stop when they touch
+
+  let d = dist(flyImage.x, flyImage.y, discoBallImage.x, discoBallImage.y);
+  if (d < discoBallImage.size/3 + flyImage.size/2) {
+    noLoop();
+  }
+
+// make the disco ball bigger sometimes and slower or smaller and faster depending on it's starting position
+
+  if (discoBallImage.y >= width * 0.4) {
+    discoBallImage.size = 200;
+    discoBallImage.speed = 1;
+  }
+  else {
+    discoBallImage.size = 300;
+    discoBallImage.speed = 8;
+  }
 }
