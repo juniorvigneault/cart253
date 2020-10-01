02e778393c64a4de0f994f44fb3737592236f956
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-01 11:48:02 -0400

    A2: Added Disco Ball

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -6,10 +6,21 @@ Here is a description of this template p5 project.
 **************************************************/
 
 let flyImage;
+let discoBallImage = {
+  x:0,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:5,
+  image: undefined
+}
 
 
 function preload() {
   flyImage = loadImage("assets/images/fly.png");
+
+  discoBallImage.image = loadImage("assets/images/discoball.png");
 }
 
 let bg = {
@@ -18,25 +29,11 @@ let bg = {
   b:0
 }
 
-let covid19 = {
-  x:0,
-  y:250,
-  size:250,
-  vx:0,
-  vy:0,
-  speed:5,
-  fill:{
-    r:255,
-    g:0,
-    b:0
-  }
-}
-
 function setup() {
 createCanvas(windowWidth, windowHeight);
 
-  covid19.y = random(0,height);
-  covid19.vx = covid19.speed;
+  discoBallImage.y = random(0,height);
+  discoBallImage.vx = discoBallImage.speed;
 
   noCursor();
 
@@ -61,21 +58,21 @@ for (let i =0; i < 10000; i++) {
   point(x,y);
 }
 
-  covid19.x = covid19.x + covid19.vx;
-  covid19.y = covid19.y + covid19.vy;
+  discoBallImage.x = discoBallImage.x + discoBallImage.vx;
+  discoBallImage.y = discoBallImage.y + discoBallImage.vy;
 
-  if (covid19.x > width) {
-    covid19.x = 0;
-    covid19.y = random(0,height);
+  if (discoBallImage.x > width) {
+    discoBallImage.x = 0;
+    discoBallImage.y = random(0,height);
   }
+
+  imageMode(CENTER);
+  image(discoBallImage.image, discoBallImage.x, discoBallImage.y, 300, 300);
 
 
   flyImage.x = mouseX;
   flyImage.y = mouseY;
 
-
-  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-  ellipse(covid19.x, covid19.y, covid19.size);
 
   imageMode(CENTER);
   image(flyImage, mouseX, mouseY,80,80);
