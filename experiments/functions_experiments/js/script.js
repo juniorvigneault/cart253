c3d6a2da2ed3a7d1b5e1fc1fba0f8fc634eeaa74
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-07 12:26:54 -0400

    EX4: finished video on functions

--- experiments/functions_experiments/js/script.js
+++ experiments/functions_experiments/js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+  let circle = {
+    x:0,
+    y:250,
+    size:100,
+    vx:1,
+    vy:0
+  };
+
 /**************************************************
 Template p5 project
 Pippin Barr
@@ -11,6 +19,7 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
 
 }
 
@@ -18,5 +27,38 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+
+  display();
+  move();
+  wrap();
+
+
 
 }
+
+  function mousePressed() {
+    reset();
+  }
+
+  function reset(){
+    circle.x = 0;
+    circle.vx = circle.vx + 2;
+    circle.size = circle.size + 5;
+  };
+
+  function display(){
+    fill(255,0,0);
+    ellipse(circle.x, circle.y, circle.size)
+  };
+
+  function move(){
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+  }
+
+  function wrap(){
+    if(circle.x > width) {
+      reset();
+    }
+  }
