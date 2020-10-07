4590f35d7ca95b5530b41be91ac1126e2547d02d
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-07 16:18:21 -0400

    EXP:4 finished experimenting with functions for now

--- experiments/functions_experiments/js/script.js
+++ experiments/functions_experiments/js/script.js
@@ -1,13 +1,5 @@
 "use strict";
 
-  let circle = {
-    x:0,
-    y:250,
-    size:100,
-    vx:1,
-    vy:0
-  };
-
 /**************************************************
 Template p5 project
 Pippin Barr
@@ -19,7 +11,7 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+  createCanvas(windowWidth,windowHeight);
 
 }
 
@@ -27,38 +19,14 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+  background(200);
 
-  display();
-  move();
-  wrap();
-
-
+  textAlign(CENTER,CENTER);
+  textSize(100);
+  textStyle(BOLD);
+  fill(random(0,255), random(0,255), random(0,255));
+  stroke(random(150,255), random(150,255), random(150,255));
+  strokeWeight(10);
+  text('Hello, world', windowWidth/2, windowHeight/2);
 
 }
-
-  function mousePressed() {
-    reset();
-  }
-
-  function reset(){
-    circle.x = 0;
-    circle.vx = circle.vx + 2;
-    circle.size = circle.size + 5;
-  };
-
-  function display(){
-    fill(255,0,0);
-    ellipse(circle.x, circle.y, circle.size)
-  };
-
-  function move(){
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-  }
-
-  function wrap(){
-    if(circle.x > width) {
-      reset();
-    }
-  }
