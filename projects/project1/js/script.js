475f123d11de3353af15161789d3419b2129ec89
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-14 19:19:13 -0400

    P1: Added more titles and a function for user to type 'i promise'

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -34,9 +34,76 @@ let flower2 = {
     b:55,
   }
 }
+let flower3 = {
+  x : 1,
+  y : 1,
+  x2 : 1,
+  y2 : 1,
+  angle : 10,
+  speed : 4,
+  weight: .2,
+  fill : {
+    r:255,
+    g:255,
+    b:255,
+  }
+}
+let flower4 = {
+  x : 1,
+  y : 1,
+  x2 : 1,
+  y2 : 1,
+  angle : 1,
+  speed : .09,
+  weight: .2,
+  fill : {
+    r:0,
+    g:0,
+    b:255,
+  }
+}
+let flower5 = {
+  x : 1,
+  y : 1,
+  x2 : 1,
+  y2 : 1,
+  angle : 29,
+  speed : .02,
+  weight: .2,
+  fill : {
+    r:80,
+    g:0,
+    b:20,
+  }
+}
+let flower6 = {
+  x : 1,
+  y : 1,
+  x2 : 1,
+  y2 : 1,
+  angle : 29,
+  speed : .02,
+  weight: .2,
+  fill : {
+    r:0,
+    g:0,
+    b:0,
+  }
+}
+
+let size = {
+  growing:50,
+}
 let fade = {
   fadeIn: 0,
-  fadeIn2: 0.5,
+  fadeIn2: .7,
+}
+
+let typing = ``;
+let everythingSFX;
+
+function preload(){
+  everythingSFX = loadSound('assets/sounds/everything.mp3')
 }
 
 let state = 'title';
@@ -52,8 +119,25 @@ function setup() {
 // draw()
 //
 // Description of draw() goes here.
+
 function draw() {
   background(0);
+  titleSwitch();
+
+}
+
+
+
+
+
+
+
+// My beautiful functions
+
+
+// This function has all my state switches everytime the title changes
+
+function titleSwitch(){
   if (state === 'title'){
   title();
 }
@@ -123,10 +207,25 @@ else if (state === 'title22'){
 else if (state === 'title23'){
   title23();
 }
+else if (state === 'title24'){
+  title24();
+}
+else if (state === 'title25'){
+  title25();
+}
+else if (state === 'title26'){
+  title26();
+}
+else if (state === 'title27'){
+  title27();
+}
+else if (state === 'title28'){
+  title28();
+}
 }
 
 
-
+// This function is for the big laser flower background
 
 function flowers(flowers){
 
@@ -145,6 +244,8 @@ push();
 }
 }
 
+// All my different titles with different back
+
 function title(){
   flowers(flower1)
   push();
@@ -155,7 +256,7 @@ function title(){
   textStyle(BOLD);
   text("What you are about to witness...", windowWidth/2, windowHeight/4);
   textAlign(LEFT, BOTTOM);
-  text("...has never been seen before in the history of programming...", windowWidth * .2, windowHeight * .6 )
+  text(`...has never been seen before in the history of programming...`, windowWidth * .2, windowHeight * .6 )
   pop();
 }
   function title2(){
@@ -166,9 +267,9 @@ function title(){
     textAlign(RIGHT);
     textSize(40);
     textStyle(BOLD);
-    text("It may change...", windowWidth /2, windowHeight/4);
+    text(`It may change...`, windowWidth /2, windowHeight/4);
     textAlign(LEFT, BOTTOM);
-    text("...everything you know about programming...", windowWidth * .3, windowHeight * .6 )
+    text(`...everything you know about programming...`, windowWidth * .3, windowHeight * .6 )
     pop();
 }
 function title3(){
@@ -180,217 +281,237 @@ function title3(){
   textAlign(CENTER,CENTER);
   textSize(size);
   textStyle(BOLD);
-  text("EVERYTHING...", windowWidth/2, windowHeight/2);
+  text(`EVERYTHING...`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title4(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(45);
   textStyle(BOLD);
-  text("Welcome to my show-stopping program!", windowWidth/2, windowHeight/2);
+  text(`Some have called this program "life changing"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title5(){
   flowers(flower1)
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(45);
   textStyle(BOLD);
-  text("Welcome to my unbelievable program!", windowWidth/2, windowHeight/2);
+  text(`Some have called this program "breathtaking"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title6(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(45);
   textStyle(BOLD);
-  text("Welcome to my remarkable program!", windowWidth/2, windowHeight/2);
+  text(`Some have called this program "prodigeous"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title7(){
-  flowers(flower1)
+flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(45);
   textStyle(BOLD);
-  text("Welcome to my breathtaking program!", windowWidth/2, windowHeight/2);
+  text(`Some have called this program "simply incredible"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title8(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(100);
+  textSize(70);
   textStyle(BOLD);
-  text("Prodigeous", windowWidth/2, windowHeight/2);
+  text(`"Marvelous"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title9(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(100);
+  textSize(90);
   textStyle(BOLD);
-  text("Dazzling", windowWidth/2, windowHeight/2);
+  text(`"Dazzling"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title10(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(100);
+  textSize(110);
   textStyle(BOLD);
-  text("Sublime", windowWidth/2, windowHeight/2);
+  text(`"Sublime"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title11(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
-  textAlign(CENTER,CENTER);
-  textSize(100);
-  textStyle(BOLD);
-  text("Beyond belief", windowWidth/2, windowHeight/2);
-  pop();
-}
-function title12(){
-  flowers(flower1)
-  push();
-  fill(255);
-  textAlign(CENTER,CENTER);
-  textSize(100);
-  textStyle(BOLD);
-  text("Charming", windowWidth/2, windowHeight/2);
-  pop();
-}
-function title13(){
-  flowers(flower1)
-  push();
-  fill(255);
-  textAlign(CENTER,CENTER);
-  textSize(100);
-  textStyle(BOLD);
-  text("Miraculous", windowWidth/2, windowHeight/2);
-  pop();
-}
-function title14(){
-  flowers(flower1)
-  push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(120);
   textStyle(BOLD);
-  text("Phenomenal", windowWidth/2, windowHeight/2);
+  text(`"Beyond"`, windowWidth/2, windowHeight/2);
   pop();
 }
-function title15(){
-  flowers(flower1)
+function title12(){
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(130);
+  textStyle(BOLD);
+  text(`"Charming"`, windowWidth/2, windowHeight/2);
+  pop();
+}
+function title13(){
+  flowers(flower1);
+  push();
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(140);
+  textStyle(BOLD);
+  text(`"Miraculous"`, windowWidth/2, windowHeight/2);
+  pop();
+}
+function title14(){
+  flowers(flower1);
+  push();
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(150);
   textStyle(BOLD);
-  text("Improbable", windowWidth/2, windowHeight/2);
+  text(`"Phenomenal"`, windowWidth/2, windowHeight/2);
+  pop();
+}
+function title15(){
+  flowers(flower1);
+  push();
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(160);
+  textStyle(BOLD);
+  text(`"Improbable"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title16(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(155);
+  textSize(165);
   textStyle(BOLD);
-  text("Implausible", windowWidth/2, windowHeight/2);
+  text(`"Implausible"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title17(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(170);
   textStyle(BOLD);
-  text("Far-fetched", windowWidth/2, windowHeight/2);
+  text(`"Ticklish"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title18(){
-  flowers(flower1)
+  flowers(flower1);
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
   textSize(185);
   textStyle(BOLD);
-  text("Embarrassing", windowWidth/2, windowHeight/2);
+  text(`"Embarrassing"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title19(){
-  flowers(flower1)
+  flowers(flower2);
   push();
-  fill(255);
+  let color = random(0,255);
+  fill(color);
   textAlign(CENTER,CENTER);
   textSize(200);
   textStyle(BOLD);
-  text("DISGRACEFUL", windowWidth/2, windowHeight/2);
+  text(`"DISGRACEFUL"`, windowWidth/2, windowHeight/2);
   pop();
 }
 function title20(){
   flowers(flower1)
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(100);
   textStyle(BOLD);
-  text("TICKLISH", windowWidth/2, windowHeight/2);
+  text("...and more...", windowWidth/2, windowHeight/2);
   pop();
 }
 function title21(){
-  flowers(flower1)
+  flowers(flower4)
   push();
-  fill(255);
+  let color = random(0,255);
+  fill(color);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(50);
   textStyle(BOLD);
-  text("CELEBRATORY!", windowWidth/2, windowHeight/2);
+  text("Are you ready?", windowWidth/2, windowHeight/2);
   pop();
 }
 function title22(){
-  flowers(flower1)
+  flowers(flower5)
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(80);
   textStyle(BOLD);
-  text("HUMDINGER!", windowWidth/2, windowHeight/2);
+  text("You must before promise...", windowWidth/2, windowHeight/2);
   pop();
 }
 function title23(){
-  flowers(flower1)
+  flowers(flower6)
   push();
-  fill(255);
+  let color = random(0,255);
+  fill(color);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(100);
   textStyle(BOLD);
-  text("WELCOME", windowWidth/2, windowHeight/2);
+  text("PROMISE TO NEVER TELL", windowWidth/2, windowHeight/2);
   pop();
 }
 function title24(){
-  flowers(flower1)
   push();
-  fill(255);
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(50);
   textStyle(BOLD);
-  text("TO", windowWidth/2, windowHeight/2);
+  text("...if you do promise...", windowWidth/2, windowHeight/2);
   pop();
 }
 function title25(){
@@ -400,17 +521,46 @@ function title25(){
   textAlign(CENTER,CENTER);
   textSize(200);
   textStyle(BOLD);
-  text("MY", windowWidth/2, windowHeight/2);
+  text("...if you do promise...", windowWidth/2, windowHeight/2);
   pop();
 }
 function title25(){
-  flowers(flower1)
+  push();
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(50);
+  textStyle(BOLD);
+  text("...then click, and type 'I promise'", windowWidth/2, windowHeight/2);
+  pop();
+}
+function title26(){
   push();
   fill(255);
   textAlign(CENTER,CENTER);
-  textSize(200);
+  textSize(50);
   textStyle(BOLD);
-  text("PROGRAM", windowWidth/2, windowHeight/2);
+  text(typing, windowWidth/2, windowHeight/2);
+  pop();
+}
+function title27(){
+  push();
+  fade.fadeIn = fade.fadeIn - fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(50);
+  textStyle(BOLD);
+  text("Good...", windowWidth/2, windowHeight/2);
+  pop();
+}
+function title28(){
+  push();
+  fade.fadeIn = fade.fadeIn + fade.fadeIn2;
+  fill(fade.fadeIn);
+  textAlign(CENTER,CENTER);
+  textSize(50);
+  textStyle(BOLD);
+  text("Very good...", windowWidth/2, windowHeight/2);
   pop();
 }
 
@@ -481,4 +631,23 @@ function mousePressed(){
   else if (state === 'title22'){
     state = 'title23';
   }
+  else if (state === 'title23'){
+    state = 'title24';
+  }
+  else if (state === 'title24'){
+    state = 'title25';
+  }
+  else if (state === 'title25'){
+    state = 'title26';
+  }
+  else if (state === 'title26'){
+    state = 'title27';
+  }
+  else if (state === 'title27'){
+    state = 'title28';
+  }
+}
+
+function keyTyped(){
+  typing = typing + key;
 }
