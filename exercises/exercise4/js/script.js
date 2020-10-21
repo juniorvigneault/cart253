e60a97490d3c6b226742c4a5564e3daa7ca32e7b
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-21 13:40:22 -0400

    E4: Wrote the original fish code from the video

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -1,22 +1,58 @@
 "use strict";
 
-/**************************************************
-Template p5 project
-Pippin Barr
+let school = [];
 
-Here is a description of this template p5 project.
-**************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
+  createCanvas(800,800);
 
+ // create 4 fish positioned randomly
+
+  school[0] = createFish(random(0, width), random(0,height));
+  school[1] = createFish(random(0, width), random(0,height));
+  school[2] = createFish(random(0, width), random(0,height));
+  school[3] = createFish(random(0, width), random(0,height));
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+// create new javascript object describing fish
 
+function createFish(x,y){
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
+
+function draw() {
+  background(0);
+}
+
+// chooses wether the provided fish changes direction and moves it
+function moveFish(fish) {
+  let change = random(0,1);
+  if (change <0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+    }
+    // move fish
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+
+    // constrain fish inside canvas
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+// display fish on canvas
+function displayFish(fish){
+  push();
+  fill(200,100,100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
 }
