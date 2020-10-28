681f49399617feea665e60cdd085906307456a19
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2020-10-27 21:45:46 -0400

    E5: Started exercise 5

---
+++ exercises/exercise5/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Exercise 5 : Juggling!
+Junior Vigneault
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
